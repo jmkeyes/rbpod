67ebfbd0d0d21fdf9d496c8d410c439589aa8e8c
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2013-10-26 16:45:55 -0700

    Define #master and #podcasts for playlist collections to access the master and podcast playlists directly.

--- ext/rbpod/playlist.c
+++ ext/rbpod/playlist.c
@@ -9,7 +9,21 @@ VALUE cRbPodPlaylist;
 VALUE mRbPodPlaylistCollection;
 
 static VALUE rbpod_playlist_collection_parent(VALUE self) {
-    return rb_iv_get(self, "@parent");;
+    return rb_iv_get(self, "@parent");
+}
+
+static VALUE rbpod_playlist_collection_podcasts_get(VALUE self) {
+    VALUE parent = rbpod_playlist_collection_parent(self);
+    Itdb_iTunesDB *database = TYPED_DATA_PTR(parent, Itdb_iTunesDB);
+    Itdb_Playlist *podcasts = itdb_playlist_podcasts(database);
+    return Data_Wrap_Struct(cRbPodPlaylist, NULL, NULL, (void *) podcasts);
+}
+
+static VALUE rbpod_playlist_collection_master_get(VALUE self) {
+    VALUE parent = rbpod_playlist_collection_parent(self);
+    Itdb_iTunesDB *database = TYPED_DATA_PTR(parent, Itdb_iTunesDB);
+    Itdb_Playlist *master = itdb_playlist_mpl(database);
+    return Data_Wrap_Struct(cRbPodPlaylist, NULL, NULL, (void *) master);
 }
 
 inline VALUE rbpod_playlist_collection_create(VALUE parent, GList *items) {
@@ -112,6 +126,10 @@ void Init_rbpod_playlist(void) {
     mRbPodPlaylistCollection = rb_define_module_under(cRbPodPlaylist, "Collection");
 
     rb_define_private_method(mRbPodPlaylistCollection, "parent", rbpod_playlist_collection_parent, 0);
+
+    /* iPod-specific methods. */
+    rb_define_method(mRbPodPlaylistCollection, "master", rbpod_playlist_collection_master_get, 0);
+    rb_define_method(mRbPodPlaylistCollection, "podcasts", rbpod_playlist_collection_podcasts_get, 0);
 
     return;
 }

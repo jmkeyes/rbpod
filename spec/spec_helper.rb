a704f3f867369bdd7d74769d3523d92d242a01e1
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2013-10-27 17:57:03 -0700

    RSpec should load the 'spec_helper' automatically.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 require 'rspec'
 require 'rbpod'
+require 'tmpdir'
 
 RSpec.configure do |config|
   config.color_enabled = true
   config.formatter     = 'documentation'
+end
+
+def within_temporary_directory(&block)
+  Dir.mktmpdir(&block)
 end

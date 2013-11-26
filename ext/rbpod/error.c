c7e699f9180c9899e5d726be331884b85e11e75b
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2013-11-25 16:02:29 -0800

    Don't allow RDoc to be confused by it's inability to parse documentation.

--- ext/rbpod/error.c
+++ ext/rbpod/error.c
@@ -16,7 +16,11 @@ inline VALUE rbpod_raise_error(GError *error)
     return Qnil;
 }
 
-void Init_rbpod_error(void) {
+void Init_rbpod_error(void)
+{
+#if RDOC_CAN_PARSE_DOCUMENTATION
+    mRbPod = rb_define_module("RbPod");
+#endif
     /* This is a generic subclass of RuntimeError for RbPod-specific errors.*/
     eRbPodError = rb_define_class_under(mRbPod, "Error", rb_eRuntimeError);
 }

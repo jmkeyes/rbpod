5f30bdb904337696a3f7e2ec29c99a708525c595
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2013-11-26 12:04:16 -0800

    Simply *Collection spec nesting.

--- spec/rbpod/playlist_collection_spec.rb
+++ spec/rbpod/playlist_collection_spec.rb
@@ -1,45 +1,44 @@
 describe RbPod::PlaylistCollection do
-  it_behaves_like RbPod::Collection do
-    # Create a temporary database for the test.
-    include_context 'a new database'
+  it_behaves_like RbPod::Collection
+  # Create a temporary database for the test.
+  include_context 'a new database'
 
-    # Use it as a parent to the playlist collection.
-    let (:collection) { RbPod::PlaylistCollection.new(database) }
+  # Use it as a parent to the playlist collection.
+  let (:collection) { RbPod::PlaylistCollection.new(database) }
 
-    describe '#database' do
-      it 'should return the parent database' do
-        collection.database.should eq(database)
-        collection.database.should be_kind_of(RbPod::Database)
-      end
+  describe '#database' do
+    it 'should return the parent database' do
+      collection.database.should eq(database)
+      collection.database.should be_kind_of(RbPod::Database)
+    end
+  end
+
+  describe '#master' do
+    it 'should return a playlist' do
+      collection.master.should be_kind_of(RbPod::Playlist)
     end
 
-    describe '#master' do
-      it 'should return a playlist' do
-        collection.master.should be_kind_of(RbPod::Playlist)
-      end
+    it 'should be marked as the master playlist' do
+      collection.master.should be_master
+    end
+  end
 
-      it 'should be marked as the master playlist' do
-        collection.master.should be_master
+  describe '#podcast' do
+    context 'on a new database' do
+      it 'should return nil' do
+        collection.podcast.should be_nil
       end
     end
+  end
 
-    describe '#podcast' do
-      context 'on a new database' do
-        it 'should return nil' do
-          collection.podcast.should be_nil
-        end
-      end
+  describe '#include?' do
+    it 'should detect the master playlist' do
+      collection.include?(collection.master).should be_true
     end
 
-    describe '#include?' do
-      it 'should detect the master playlist' do
-        collection.include?(collection.master).should be_true
-      end
-
-      it 'should not detect a new playlist' do
-        anonymous_playlist = RbPod::Playlist.new()
-        collection.include?(anonymous_playlist).should be_false
-      end
+    it 'should not detect a new playlist' do
+      anonymous_playlist = RbPod::Playlist.new()
+      collection.include?(anonymous_playlist).should be_false
     end
   end
 end
--- spec/rbpod/track_collection_spec.rb
+++ spec/rbpod/track_collection_spec.rb
@@ -1,16 +1,15 @@
 describe RbPod::TrackCollection do
-  it_behaves_like RbPod::Collection do
-    # A track collection only requires a playlist, so we don't need a database.
-    let(:playlist) { RbPod::Playlist.new() }
+  it_behaves_like RbPod::Collection
+  # A track collection only requires a playlist, so we don't need a database.
+  let(:playlist) { RbPod::Playlist.new() }
 
-    # Use the new playlist as a parent for this track collection.
-    let(:collection) { described_class.new(playlist) }
+  # Use the new playlist as a parent for this track collection.
+  let(:collection) { described_class.new(playlist) }
 
-    describe '#playlist' do
-      it 'should return the parent playlist' do
-        collection.playlist.should eq(playlist)
-        collection.playlist.should be_kind_of(RbPod::Playlist)
-      end
+  describe '#playlist' do
+    it 'should return the parent playlist' do
+      collection.playlist.should eq(playlist)
+      collection.playlist.should be_kind_of(RbPod::Playlist)
     end
   end
 end

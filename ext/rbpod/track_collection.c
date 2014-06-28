15a2bb7e75c0dec230c6c01b6cc60b3198b53a9e
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2014-06-28 11:48:13 -0700

    Support adding/removing tracks from RbPod::TrackCollection.

--- ext/rbpod/track_collection.c
+++ ext/rbpod/track_collection.c
@@ -10,12 +10,95 @@
  */
 static VALUE rbpod_track_collection_playlist(VALUE self)
 {
-    return rb_iv_get(self, "@playlist");
+    VALUE playlist = rb_iv_get(self, "@playlist");
+
+    if (rb_obj_is_instance_of(playlist, cRbPodPlaylist) == FALSE) {
+        rb_raise(eRbPodError, "Invalid Arguments: Expected RbPod::Playlist, got %s", StringValueCStr(playlist));
+        return Qnil;
+    }
+
+    return playlist;
 }
 
 /*
  * call-seq:
- *     size() -> Integer
+ *     include?(track) -> boolean
+ *
+ * Determines if the given RbPod::Track +track+ is within this playlist.
+ */
+static VALUE rbpod_track_collection_include(VALUE self, VALUE track)
+{
+    VALUE playlist = rbpod_track_collection_playlist(self);
+    Itdb_Playlist *_playlist = TYPED_DATA_PTR(playlist, Itdb_Playlist);
+    Itdb_Track *_track = TYPED_DATA_PTR(track, Itdb_Track);
+
+    if (rb_obj_is_instance_of(track, cRbPodTrack) == FALSE) {
+        rb_raise(eRbPodError, "Invalid Arguments: Expected RbPod::Track, got %s", StringValueCStr(playlist));
+        return Qnil;
+    }
+
+    return BooleanValue(itdb_playlist_contains_track(_playlist, _track));
+}
+
+/*
+ * call-seq:
+ *     remove(track) -> nil
+ *
+ * Removes a given RbPod::Track +track+ from the playlist.
+ */
+static VALUE rbpod_track_collection_remove(VALUE self, VALUE track)
+{
+    VALUE playlist = rbpod_track_collection_playlist(self);
+    Itdb_Playlist *_playlist = TYPED_DATA_PTR(playlist, Itdb_Playlist);
+    Itdb_Track *_track = TYPED_DATA_PTR(track, Itdb_Track);
+
+    if (rb_obj_is_instance_of(track, cRbPodTrack) == FALSE) {
+        rb_raise(eRbPodError, "Invalid Arguments: Expected RbPod::Track, got %s", StringValueCStr(playlist));
+        return Qnil;
+    }
+
+    itdb_playlist_remove_track(_playlist, _track);
+
+    return Qnil;
+}
+
+/*
+ * call-seq:
+ *     insert(track, position = -1) -> nil
+ *
+ * Adds a given RbPod::Track +track+ to the playlist at +position+. If +position+
+ * was not supplied, +track+ will be appended to the end of the playlist.
+ */
+static VALUE rbpod_track_collection_insert(int argc, VALUE *argv, VALUE self)
+{
+    VALUE playlist = rbpod_track_collection_playlist(self);
+    Itdb_Playlist *_playlist = TYPED_DATA_PTR(playlist, Itdb_Playlist);
+    VALUE track = Qnil, position = Qnil;
+    Itdb_Track *_track = NULL;
+    gint32 _position = 0;
+
+    if (rb_scan_args(argc, argv, "11", &track, &position) < 1) {
+        rb_raise(eRbPodError, "Invalid arguments: Expected >= 1 argument!");
+        return Qnil;
+    }
+
+    _track    = TYPED_DATA_PTR(track, Itdb_Track);
+    _position = NIL_P(position) ? -1 : NUM2INT(position);
+
+    /* We we given a track? */
+    if (rb_obj_is_instance_of(track, cRbPodTrack) == FALSE) {
+        rb_raise(eRbPodError, "Invalid argument: expected RbPod::Track, got %s", StringValueCStr(playlist));
+        return Qnil;
+    }
+
+    itdb_playlist_add_track(_playlist, _track, _position);
+
+    return Qnil;
+}
+
+/*
+ * call-seq:
+ *     size() -> integer
  *
  * Returns the total number of tracks in this playlist.
  */
@@ -72,6 +155,15 @@ void Init_rbpod_track_collection(void)
     rb_define_method(cRbPodTrackCollection, "playlist", rbpod_track_collection_playlist, 0);
 
     rb_define_method(cRbPodTrackCollection, "size", rbpod_track_collection_size, 0);
+
+    rb_define_method(cRbPodTrackCollection, "insert", rbpod_track_collection_insert, -1);
+
+    rb_define_method(cRbPodTrackCollection, "remove", rbpod_track_collection_remove, 1);
+
+    rb_define_method(cRbPodTrackCollection, "include?", rbpod_track_collection_include, 1);
+
+    /* Syntactic sugar for adding a track. */
+    rb_define_alias(cRbPodTrackCollection, "<<", "insert");
 
     return;
 }

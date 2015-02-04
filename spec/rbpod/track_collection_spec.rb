4223f2c2799aec48c39cb1eca4ade7c94bbc7bbb
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2015-02-03 20:37:01 -0800

    Add pending tests for TrackCollection #insert and #remove.

--- spec/rbpod/track_collection_spec.rb
+++ spec/rbpod/track_collection_spec.rb
@@ -1,5 +1,6 @@
 describe RbPod::TrackCollection do
   it_behaves_like RbPod::Collection
+
   # A track collection only requires a playlist, so we don't need a database.
   let(:playlist) { RbPod::Playlist.new() }
 
@@ -10,6 +11,18 @@ describe RbPod::TrackCollection do
     it 'should return the parent playlist' do
       collection.playlist.should eq(playlist)
       collection.playlist.should be_kind_of(RbPod::Playlist)
+    end
+  end
+
+  describe '#insert' do
+    it 'should add a track to the playlist' do
+      skip 'not implemented yet'
+    end
+  end
+
+  describe '#remove' do
+    it 'should remove a track from the playlist' do
+      skip 'not implemented yet'
     end
   end
 end

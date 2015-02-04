3aa5aab3ab8d46ae48059a02f19e2c6455f2a6ce
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2015-02-03 20:37:54 -0800

    Modify spec_helper to allow loading sample data.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@ require 'tmpdir'
 
 require 'support/shared_contexts'
 
+SPEC_DIR = File.dirname(__FILE__)
+
 def within_temporary_directory(&block)
   Dir.mktmpdir(&block)
+end
+
+def sample_data_file(file)
+  File.join(SPEC_DIR, 'data', file)
 end
